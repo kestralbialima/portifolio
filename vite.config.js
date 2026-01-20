7bc337af28e6bea77da2d38fd4fba637d27cb9d0
kestralbialima <biancal399@gmail.com>
2026-01-20 14:23:07 -0300

    fix: final sync com o nome portifolio

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/portifolio/', 
+  base: '/portifolio/',
   build: {
     outDir: 'docs',
     emptyOutDir: true, // Isso limpa a pasta docs a cada novo build
