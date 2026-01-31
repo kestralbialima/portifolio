99705726cc313723701c63483228e771888c7a99
kestralbialima <biancal399@gmail.com>
2026-01-31 14:02:30 -0300

    fix: permitindo hosts e corrigindo quebra de linha

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/',
-  
+  server: {
+    allowedHosts: [
+      'devserver-main--portifolio-corebuild.netlify.app',
+      'portifolio-corebuild.netlify.app'
+    ]
+  }
 })
