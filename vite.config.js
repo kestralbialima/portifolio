8875414c75a735322787b055895207ed138e937c
kestralbialima <biancal399@gmail.com>
2026-04-07 11:45:32 -0300

    ci: configura deploy automático via github actions

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // Alterado de react-swc para plugin-react
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portifolio/', // Mantemos a base corrigida para o GitHub Pages
+  base: '/portifolio/', 
 })
